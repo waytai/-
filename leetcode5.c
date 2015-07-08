35f7e232eb5e739dcae70553c69adfec8531fba7
wayne <278972894@qq.com>
2015-07-08 14:08:35 +0800

    leetcode5.c

--- leetcode5.c
+++ leetcode5.c
@@ -9,53 +9,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int **generate(int numrows, int **columnsize)
+int **generate(int numrows, int **columnsize, int *returnsize)
 {
     int i,j;
+    *returnsize = numrows;
+    printf("--- %d\n", *returnsize);
+    printf("--- %lu\n", sizeof(int*)*(*returnsize));
+    printf("--- %lu\n", sizeof(int)*(*returnsize));
+    int **ans = (int **)malloc(sizeof(int*)*(*returnsize));
+    (*columnsize) = (int *)malloc(sizeof(int)*(*returnsize));
+
     for(i=0; i<numrows; i++)
     {
-        //*(columnsize+i)= calloc((i+1), sizeof(int));
-        **(columnsize+i)= (int **)malloc((i+1)*sizeof(int));
-        for(j=0; j<i+1; j++)
+        (*columnsize)[i] =  i+1;
+        ans[i] = (int *)malloc(sizeof(int)*((*columnsize)[i]));
+        ans[i][0] = 1;
+        for(j=0; j<i; j++)
         {
-            //(columnsize+i)[j]  = (int *)calloc((j+1), sizeof(int));
-            (columnsize+i)[j]  = (int *)malloc((j+1)*sizeof(int));
-            *(columnsize+i)[0] = 1;
-            *(columnsize+i)[i] = 1;
-            if(j>0 && j<i)
-            {
-            *(columnsize+i)[j] = *(columnsize+i-1)[j-1] +*(columnsize+i-1)[j];
-            printf("%d\n",*(columnsize+i)[j]);
-            }
+            ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+          }
+        ans[i][i] = 1; 
         }
-    }
-    return columnsize;
-}
 
-void print_ab(int **ab, int len)
-{
-    int i,j, t;
-    for(i=0; i<len; i++)
-    {
-        for(j=0; j<i+1; j++)
-        {
-            t = *(ab+i)[j];
-            printf("%d\n", t);
-        }
-        printf("\n");
-
+    return ans;
     }
 
-    }
 
 int main()
 {
-    int **ab;
-    int len = 5;
-    ab = generate(len, ab);
-    print_ab(ab, len);
-    printf("%d\n", len);
-    printf("\n");
-     return 0;
+    int returnsize = 0;
+    int *columnsize = NULL;
+    int **ret = generate(6, &columnsize, &returnsize);
+
+    int i, j;
+    for(i=0; i < returnsize; i++)
+    {
+        for(j=0; j<columnsize[i]; j++)
+        {
+            printf("%d ", ret[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
 
