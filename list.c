b2fbf3ce347056afc592a53cf9c50b29f895e1ba
wayne <278972894@qq.com>
2015-07-13 16:22:14 +0800

    add list

--- list.c
+++ list.c
@@ -26,21 +26,65 @@ void insert_list_tail(list_s *list_p, int insert_data)
     list_p->next = tmp;  
 }
 
+
+void insert_nth(list_s *list_p, int n, int ele)
+{
+    int t=0;
+    list_s *tmp = (list_s *)malloc(sizeof(list_s));
+    tmp->data = ele;
+    while(t<n)
+    {
+        list_p = list_p->next;
+        t++;
+    }
+    tmp->next = list_p->next;
+    list_p->next = tmp;
+}
+
 int main()
 {
     list_s *list_b;
     list_b = (list_s *)malloc(sizeof(list_s));
     list_b->data = 100;
+    list_s *list_c;
+    list_c = list_b;
     int i;
     for(i=0; i<10; i++)
     {
         insert_list_tail(list_b, i);
     }
 
+    int length;
+    length =0;
     while(list_b->next != NULL)
     {
-        printf("%d\n", list_b->data);
+        printf("%d  ", list_b->data);
+        length++;
         list_b = list_b->next;
     }
+    printf("%d\n", list_b->data);
+    printf("length of list is %d\n", ++length);
+
+    insert_nth(list_c, 2, 90);
+
+
+    while(list_c->next != NULL)
+    {
+        printf("%d  ", list_c->data);
+        list_c = list_c->next;
+    }
+    printf("%d\n", list_c->data);
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
