d989a843d5e7f738da85d4c88f252350e720a304
wayne <278972894@qq.com>
2015-07-16 17:42:49 +0800

    same_tree.c

---
+++ same_tree.c
@@ -0,0 +1,30 @@
+/*************************************************************************
+    > File Name: same_tree.c
+    > Author: wayne
+    > Mail: @163.com 
+    > Created Time: 2015年07月15日 星期三 16时07分37秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct TreeNode *treenode;
+struct TreeNode
+{
+    int val;
+    treenode left;
+    treenode right;
+};
+
+
+void insert(treenode L, int i)
+{
+
+}
+
+int main()
+{
+
+    return 0;
+}
