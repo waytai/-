4c8e5e8b54613d9c8af7701ce300bceaf716ac5f
wayne <278972894@qq.com>
2015-07-17 16:55:25 +0800

    same_tree.c

--- same_tree.c
+++ same_tree.c
@@ -18,13 +18,76 @@ struct TreeNode
 };
 
 
-void insert(treenode L, int i)
+treenode newnode(int data)
 {
+    treenode node = (treenode)malloc(sizeof(struct TreeNode));
+    //struct TreeNode *node = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    node->val = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
 
+void printpostorder(treenode node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+    printpostorder(node->left);
+    printpostorder(node->right);
+    printf("%d ", node->val);
+}
+
+void printmidorder(treenode node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+    printpostorder(node->left);
+    printf("%d ", node->val);
+    printpostorder(node->right);
+}
+
+
+int same_tree(treenode a, treenode b)
+{
+    if(a==NULL && b==NULL)
+    {
+        return 1;
+    }
+    else 
+    {
+        if(a->val== b->val)
+        {
+            return same_tree(a->left, b->left)&&same_tree(a->right, b->right);
+        }
+        return 0;
+    }
 }
 
 int main()
 {
+    treenode root = newnode(1);
+    root->left = newnode(2);
+    root->right = newnode(3);
+    root->left->left = newnode(4);
+    root->left->right = newnode(5);
 
+    printpostorder(root);
+    printf("\n");
+    treenode same_root = newnode(1);
+    same_root->left = newnode(2);
+    same_root->right = newnode(3);
+    same_root->left->left = newnode(4);
+    same_root->left->right = newnode(5);
+    printpostorder(same_root);
+    printf("\n");
+
+    int same = same_tree(root, same_root);
+    printf("%d\n", same);
     return 0;
 }
+
+
