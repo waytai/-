068ae0fef5e4e8287e0d40dd98f8a30ad7f4a3c0
wayne <278972894@qq.com>
2015-07-21 11:17:10 +0800

    max_depth_btree.c

---
+++ max_depth_btree.c
@@ -0,0 +1,85 @@
+/*************************************************************************
+    > File Name: max_depth_btree.c
+    > Author: wayne
+    > Mail: @163.com 
+    > Created Time: 2015年07月21日 星期二 10时38分46秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct TreeNode *treenode;
+struct TreeNode
+{
+    int val;
+    treenode left;
+    treenode right;
+};
+
+
+treenode newnode(int data)
+{
+    treenode node = (treenode)malloc(sizeof(struct TreeNode));
+    //struct TreeNode *node = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    node->val = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+void printpostorder(treenode node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+    printpostorder(node->left);
+    printpostorder(node->right);
+    printf("%d ", node->val);
+}
+
+
+int max_depth_tree(treenode node)
+{
+    int num = 0;
+    if(!node)
+    {
+        return 0;
+    }
+    else
+    {
+        num = traval(node);
+        return num;
+    }
+}
+
+
+int traval(treenode node)
+{
+    if(!node->left && !node->right)
+    {
+        return 1;
+    }
+    else if(node->left)
+    {
+        return traval(node->left)+1;
+    }
+    else
+    {
+        return traval(node->right)+1;
+    }
+}
+
+int main()
+{
+    treenode root = newnode(1);
+    root->left = newnode(2);
+    root->right = newnode(3);
+    root->left->left = newnode(4);
+    root->left->right = newnode(5);
+    printpostorder(root);
+    printf("\n");
+    printf("%d\n", traval(root));
+    return 0;
+}
