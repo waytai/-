635b1bbce0e0edef08924f4ff99762f9da60c6b4
wayne <278972894@qq.com>
2016-09-07 11:04:04 +0800

    insert sort

--- sort.py
+++ sort.py
@@ -6,7 +6,7 @@
 
 a = [7,2,8,98,1]
 
-def buble_sort(alist):
+def bubble_sort(alist):
     len_list = len(alist)
     for i in range(len_list):
         for j in range(i+1,len_list):
@@ -14,4 +14,21 @@ def buble_sort(alist):
                 a[i],a[j] = a[j],a[i]
     return alist
 
-print buble_sort(a)
+print bubble_sort(a)
+
+
+def insert_sort(alist):
+    len_list = len(alist)
+    for i in range(len_list):
+        index = a[i]
+        for j in range(i+1, len_list):
+            if a[j] > index:
+                a[i], a[j] = a[j], a[i]
+            else:
+                index = a[j]
+    return alist
+
+print insert_sort(a)
+
+
+                
